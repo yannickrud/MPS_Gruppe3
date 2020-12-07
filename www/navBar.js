3776a76e30960a9df326089bc7ff62c9496172d2
Yannick Rudolf <>
2020-12-07 21:47:03 +0100

    Slideshow fuer die jeweiligen Themenfelder

--- www/navBar.js
+++ www/navBar.js
@@ -1,13 +1,12 @@
-// When the user scrolls the page, execute myFunction 
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
 function stickyNavigationBar() {
     // Get the navbar
     var navbar = document.getElementById("navbar");
 
     // Get the offset position of the navbar
     var sticky = navbar.offsetTop;
-
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
     } else {
@@ -15,4 +14,4 @@ function stickyNavigationBar() {
     }
 }
 
-window.onscroll = function () {stickyNavigationBar(); };
+window.onscroll = function () {stickyNavigationBar()};

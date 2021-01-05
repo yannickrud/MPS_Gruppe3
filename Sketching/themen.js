f3b3de58803a1b216c8c3803db4ad5ff73a5e4ec
daniel-s123 <74418313+daniel-s123@users.noreply.github.com>
2021-01-05 20:20:39 +0100

    Sketching Aufgaben fertig

--- Sketching/themen.js
+++ Sketching/themen.js
@@ -1,29 +1,25 @@
+let aufgaben =[
 
+     "Zitat: Kein Mensch ist so beschäftigt, dass er nicht die Zeit hat, überall zu erzählen, wie beschäftigt er ist.",
+     "Coronavirus",
+     "Trump",
+     "Zitat: Man ist meistens nur durch Nachdenken unglücklich",
+     "Tierversuche (Sollen Tierversuche verboten werden?)",
+     "Impfpflicht (Brauchen wir eine Impfpflicht?)",
+     "Brauchen wir den 6-Stunden-Arbeitstag?",
+     "US-Wahl 2020",
+     "Digitalisierung",
+     "Brexit",
+     "WHO",
+     "Zitat: Die Leute streiten im allgemeinen nur deshalb, weil sie nicht diskutieren können.",
+     "Klimawandel",
+     "Lockdown",
+     "Social Media",
+     "Wirecard Skandal",
+     "George Floyd",
+]
 
-        // Thumbnail image controls
-        //function currentAufgaben(n) {
-             //showAufgaben(AufgabenIndex = n);
-        //}
-
-        function showAufgaben(AufgabenIndex) {
-            var AufgabenIndex = Math.floor(Math.random() * 15);     // returns a random integer from 0 to 14;
-            //var i;
-            //var Aufgaben = document.getElementsByClassName("myAufgaben");
-            //var dots = document.getElementsByClassName("dot");
-            if (AufgabenIndex = 0) {alert("Skizziere Folgendes Thema: Kein Mensch ist so beschäftigt, dass er nicht die Zeit hat, überall zu erzählen, wie beschäftigt er ist.") } 
-            if (AufgabenIndex = 1) {alert("Skizziere Folgendes Thema: Coronavirus") }
-            if (AufgabenIndex = 2) {alert("Skizziere Folgendes Thema: Tierversuche (Sollen Tierversuche verboten werden?") }
-            if (AufgabenIndex = 3) {alert("Skizziere Folgendes Thema: Impfpflicht (Brauchen wir eine Impfpflicht?)") }
-            if (AufgabenIndex = 4) {alert("Skizziere Folgendes Thema: Brauchen wir den 6-Stunden-Arbeitstag?") }
-            if (AufgabenIndex = 5) {alert("Skizziere Folgendes Thema: Trump") }
-            if (AufgabenIndex = 6) {alert("Skizziere Folgendes Thema: US-Wahl 2020") }
-            if (AufgabenIndex = 7) {alert("Skizziere Folgendes Thema: Digitalisierung") }
-            if (AufgabenIndex = 8) {alert("Skizziere Folgendes Thema: Brexit") }
-            if (AufgabenIndex = 9) {alert("Skizziere Folgendes Thema: WHO") }
-            if (AufgabenIndex = 10) {alert("Skizziere Folgendes Thema: Klimawandel") }
-            if (AufgabenIndex = 11) {alert("Skizziere Folgendes Thema: Lockdown") }
-            if (AufgabenIndex = 12) {alert("Skizziere Folgendes Thema: Social Media") }
-            if (AufgabenIndex = 13) {alert("Skizziere Folgendes Thema: Wirecard Skandal") }
-            if (AufgabenIndex = 14) {alert("Skizziere Folgendes Thema: George Floyd") 
-            
-             }
+        function showAufgaben() {
+            var AufgabenIndex = Math.floor(Math.random() * aufgaben.length);     // returns a random integer from 0 to 14;
+            document.getElementById("Aufgaben").innerHTML = aufgaben[AufgabenIndex];
+        }

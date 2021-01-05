ba0ce31f7e26c349d12822c8cae80b2e81021029
daniel-s123 <74418313+daniel-s123@users.noreply.github.com>
2021-01-05 12:26:20 +0100

    Aufgabenthemen bearbeitet
    Aufgabenthemen bearbeitet; funktioniert aber immer noch nicht

--- Sketching/themen.js
+++ Sketching/themen.js
@@ -1,28 +1,29 @@
-var AufgabenIndex = 1;
-        showAufgabe(AufgabenIndex);
 
-        // Next/previous controls
-        function plusAufgabe(n) {
-            showAufgabe(AufgabenIndex += n);
-        }
 
         // Thumbnail image controls
-         function currentAufgaben(n) {
-             showAufgaben(AufgabenIndex = n);
-        }
+        //function currentAufgaben(n) {
+             //showAufgaben(AufgabenIndex = n);
+        //}
 
-        function showAufgaben(n) {
-            var i;
-            var Aufgaben = document.getElementsByClassName("myAufgaben");
-            var dots = document.getElementsByClassName("dot");
-            if (n > Aufgaben.length) {AufgabenIndex = 1} 
-            if (n < 1) {AufgabenIndex = Aufgaben.length}
-            for (i = 0; i < Aufgaben.length; i++) {
-                Aufgaben[i].style.display = "none"; 
-             }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            Aufgaben[AufgabenIndex-1].style.display = "block"; 
-                dots[AufgabenIndex-1].className += " active";
+        function showAufgaben(AufgabenIndex) {
+            var AufgabenIndex = Math.floor(Math.random() * 15);     // returns a random integer from 0 to 14;
+            //var i;
+            //var Aufgaben = document.getElementsByClassName("myAufgaben");
+            //var dots = document.getElementsByClassName("dot");
+            if (AufgabenIndex = 0) {alert("Skizziere Folgendes Thema: Kein Mensch ist so beschäftigt, dass er nicht die Zeit hat, überall zu erzählen, wie beschäftigt er ist.") } 
+            if (AufgabenIndex = 1) {alert("Skizziere Folgendes Thema: Coronavirus") }
+            if (AufgabenIndex = 2) {alert("Skizziere Folgendes Thema: Tierversuche (Sollen Tierversuche verboten werden?") }
+            if (AufgabenIndex = 3) {alert("Skizziere Folgendes Thema: Impfpflicht (Brauchen wir eine Impfpflicht?)") }
+            if (AufgabenIndex = 4) {alert("Skizziere Folgendes Thema: Brauchen wir den 6-Stunden-Arbeitstag?") }
+            if (AufgabenIndex = 5) {alert("Skizziere Folgendes Thema: Trump") }
+            if (AufgabenIndex = 6) {alert("Skizziere Folgendes Thema: US-Wahl 2020") }
+            if (AufgabenIndex = 7) {alert("Skizziere Folgendes Thema: Digitalisierung") }
+            if (AufgabenIndex = 8) {alert("Skizziere Folgendes Thema: Brexit") }
+            if (AufgabenIndex = 9) {alert("Skizziere Folgendes Thema: WHO") }
+            if (AufgabenIndex = 10) {alert("Skizziere Folgendes Thema: Klimawandel") }
+            if (AufgabenIndex = 11) {alert("Skizziere Folgendes Thema: Lockdown") }
+            if (AufgabenIndex = 12) {alert("Skizziere Folgendes Thema: Social Media") }
+            if (AufgabenIndex = 13) {alert("Skizziere Folgendes Thema: Wirecard Skandal") }
+            if (AufgabenIndex = 14) {alert("Skizziere Folgendes Thema: George Floyd") 
+            
              }
